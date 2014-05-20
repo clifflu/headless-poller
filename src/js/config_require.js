f75490ea150f4a84de76598caa1a400a150978c5
Cliff Chao-kuan Lu <clifflu@gmail.com>
2014-05-21 00:01:09 +0800

    Serve js libraries locally

--- src/js/config_require.js
+++ src/js/config_require.js
@@ -14,31 +14,42 @@ var require = {
     },
     paths: {
         'angular-1.2.16' : [
-            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min'
+            '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min',
+            'lib/angular.min'
         ],
         'angular-cookies-1.2.16' : [
             '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-cookies.min',
+            'lib/angular-cookies.min'
         ],
         'angular-resource-1.2.16' : [
             '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-resource.min',
+            'lib/angular-resource.min'
         ],
         'angular-route-1.2.16' : [
             '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.min',
+            'lib/angular-route.min'
         ],
-        'amzn-login': 'https://api-cdn.amazon.com/sdk/login1',
+        'amzn-login': [
+            'https://api-cdn.amazon.com/sdk/login1',
+            'lib/amzn-login1'
+        ],
         'aws-2.0.0-rc14': [
-            'https://sdk.amazonaws.com/js/aws-sdk-2.0.0-rc.14.min'
+            '//sdk.amazonaws.com/js/aws-sdk-2.0.0-rc.14.min',
+            'lib/aws-sdk-2.0.0-rc.14.min'
         ],
         'fb': '//connect.facebook.net/en_US/all',
         'g-plus-one': 'https://apis.google.com/js/client:plusone',
         'init': [
-            'init-built', 'init'
+            'init-built', 
+            'init'
         ],
         'jquery-1.11.0': [
             '//code.jquery.com/jquery-1.11.0.min',
+            'lib/jquery-1.11.0.min'
         ],
         'jq-throttle-1.1': [
             '//cdnjs.cloudflare.com/ajax/libs/jquery-throttle-debounce/1.1/jquery.ba-throttle-debounce.min',
+            'lib/jquery.ba-throttle-debounce.min'
         ]
     },
     shim: {
